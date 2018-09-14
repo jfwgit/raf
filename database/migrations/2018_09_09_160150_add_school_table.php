81b355c920c194d4fd91aa63321929b44cfd80ae
Dima Stelmakh <d.stelmakh@andersenlab.com>
2018-09-14 15:12:01 +0300

    Added core migrations and seeders

--- database/migrations/2018_09_09_160150_add_school_table.php
+++ database/migrations/2018_09_09_160150_add_school_table.php
@@ -1,12 +1,10 @@
 <?php
-
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
-class CreateUsersTable extends Migration
+class AddSchoolTable extends Migration
 {
-    private static $tableName = 'users';
+    private static $tableName = 'schools';
     /**
      * Run the migrations.
      *
@@ -16,10 +14,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create(static::$tableName, function (Blueprint $table): void {
             $table->increments('id');
-            $table->string('code')->nullable();
-            $table->string('email')->nullable();
-            $table->string('password')->nullable();
-            $table->smallInteger('role')->nullable();
+            $table->string('name');
+            $table->string('location');
+            $table->mediumText('data');
+            $table->string('code');
+            $table->integer('teachers');
         });
     }
     /**
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+            'role' => \App\User::ADMIN_ROLE_ADMIN
+        ]);
+    }
+}
